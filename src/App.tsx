fe531008718dadf64e110c427f348135aadf0a41
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-08-02 21:09:31 +0900

    #7 chore: install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router";
+
+//react router dom 테스트용 개발 시 삭제
+function Home() {
+  return <div>home</div>;
+}
+//react router dom 테스트용 개발 시 삭제
+function Post() {
+  return <div>post</div>;
+}
+
 function App() {
-  return <>hi</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post" element={<Post />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -10,9 +11,11 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 );
