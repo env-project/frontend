337bdf0e5191182db275fd8ac4ac362a8a0630be
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-08-03 13:02:16 +0900

    #9 feat: 커스텀 Text 컴포넌트
    variant 종류: mainText, subText, label, tooltip

---
+++ src/components/text/Text.tsx
@@ -0,0 +1,26 @@
+import type { ComponentPropsWithoutRef, ReactNode } from "react";
+
+interface TextProps extends ComponentPropsWithoutRef<"span"> {
+  variant?: "mainText" | "subText" | "label" | "tooltip";
+  children: ReactNode;
+}
+
+export default function Text({
+  variant = "mainText",
+  children,
+  className = "",
+  ...rest
+}: TextProps) {
+  const baseClass = {
+    mainText: "font-normal text-sm sm:text-base",
+    subText: "font-normal text-xs sm:text-sm",
+    label: "font-medium text-xs",
+    tooltip: "font-normal text-[11px]",
+  }[variant];
+
+  return (
+    <span className={`${baseClass} ${className}`} {...rest}>
+      {children}
+    </span>
+  );
+}
