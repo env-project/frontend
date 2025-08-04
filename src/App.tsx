f7d81be8dc4b0e8626a19b355f332ff02535af59
Jun <nbc7799@naver.com>
2025-08-04 15:54:03 +0900

    #18 feat/Footer제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import { Routes, Route } from "react-router";
-import Text from "@/components/text/Text";
-
-//react router dom 테스트용 개발 시 삭제
-function Home() {
-  return (
-    <div>
-      <Footer />
-    </div>
-  );
-}
-//react router dom 테스트용 개발 시 삭제
-function Post() {
-  return <div>post</div>;
-}
+import Footer from "./components/Footer";
+import "./index.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post" element={<Post />} />
+      <Route path="/" element={<Footer />} />
     </Routes>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import { AiOutlineFacebook } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa6";
+import { SiKakaotalk } from "react-icons/si";
+import Text from "./text/Text";
+import type { JSX } from "react";
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className="bg-bg-primary text-sm absolute bottom-0 w-full">
+      <div className="grid gap-8 md:grid-cols-2 px-10 py-6 w-full md:px-20">
+        <div className="flex justify-center md:justify-normal gap-20 ">
+          <AiOutlineFacebook size={28} />
+          <FaInstagram size={28} />
+          <SiKakaotalk size={28} />
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <Text variant="label" className="block md:text-sm">
+              .ENV
+            </Text>
+            <Text variant="label" className="block md:text-sm">
+              FE: 이재현, 김민창, 박준규
+            </Text>
+            <Text variant="label" className="block md:text-sm">
+              BE: 정상우, 김혜진, 은정연
+            </Text>
+            <Text variant="label" className="block md:text-sm">
+              주소 : OZ코딩스쿨
+            </Text>
+          </div>
+          <div className="flex gap-2">
+            <Text variant="label" className="md:text-sm">
+              <a href="#">Privacy Policy</a>
+            </Text>
+            <Text>|</Text>
+            <Text variant="label" className="md:text-sm">
+              <a href="#">Terms</a>
+            </Text>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
