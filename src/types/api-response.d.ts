9a05eda5d9bf840ed01790726c67c72bf44f305d
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-05 10:40:00 +0900

    #30 type: MyUserInfo type
    type for GET /api/v1/users/me

---
+++ src/types/api-response.d.ts
@@ -0,0 +1,40 @@
+// GET /api/v1/users/me
+interface MyUserInfo {
+  id: string;
+  email: string;
+  nickname: string;
+  profile: Profile;
+}
+
+interface Profile {
+  image_url: string;
+  is_public: boolean;
+  regions: Region[];
+  positions: PositionAndLevel[];
+  genres: Genre[];
+}
+
+interface PositionAndLevel {
+  position: Position;
+  experience_level: ExperienceLevel;
+}
+
+interface Position {
+  id: string;
+  name: string;
+}
+
+interface Region {
+  id: string;
+  name: string;
+}
+
+interface Genre {
+  id: string;
+  name: string;
+}
+
+interface ExperienceLevel {
+  id: string;
+  name: string;
+}
