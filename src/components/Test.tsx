6ff002e273b70c8ee026b58201944fb87a86c58d
Jun <nbc7799@naver.com>
2025-08-05 15:19:08 +0900

    #31 feat/error 상태 추가및 placeholder 스타일 지정

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
-import Input from "@/components/input/Input";
+import { useState } from "react";
+import AuthInput from "@/components/input/AuthInput";
 
 export default function Test() {
   const [text, setText] = useState("");
 
   return (
     <div>
-      <Input
+      <AuthInput
         type="password"
         placeholder="비밀번호를 입력해주세요"
+        error="비밀번호가 일치하지 않습니다."
         value={text}
         onChange={(e) => {
           setText(e.target.value);
---
+++ src/components/input/AuthInput.tsx
@@ -0,0 +1,54 @@
+import { useState, type ComponentPropsWithoutRef } from "react";
+import clsx from "clsx";
+import { IoEyeSharp } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa";
+import Text from "@/components/text/Text";
+
+interface InputProps extends ComponentPropsWithoutRef<"input"> {
+  type?: "text" | "password" | "email" | "number";
+  error?: string;
+}
+
+export default function AuthInput({
+  className,
+  type = "text",
+  error,
+  placeholder,
+  ...rest
+}: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => setShowPassword((prev) => !prev);
+
+  let currentType = type;
+  if (type === "password") {
+    currentType = showPassword ? "text" : "password";
+  }
+
+  return (
+    <div className="flex flex-col gap-1">
+      <div
+        className={clsx(
+          "flex bg-bg-secondary text-text-primary w-[325px]  md:w-[500px] p-3 rounded-lg ",
+          error && "border border-error"
+        )}
+      >
+        <input
+          type={currentType}
+          placeholder={placeholder}
+          className={clsx(
+            "focus:outline-none w-full border-none bg-transparent placeholder-text-primary",
+
+            className
+          )}
+          {...rest}
+        />
+        {type === "password" && (
+          <button type="button" onClick={togglePassword} className="cursor-pointer">
+            {showPassword ? <FaEyeSlash /> : <IoEyeSharp />}
+          </button>
+        )}
+      </div>
+      {error && <Text className="text-error text-sm">{error}</Text>}
+    </div>
+  );
+}
