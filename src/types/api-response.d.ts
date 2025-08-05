2a9a9a3886394658854b4aa187ee961056019fce
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-05 11:26:49 +0900

    #30 type: PostList type
    for GET /api/v1/recruiting-posts api

--- src/types/api-response.d.ts
+++ src/types/api-response.d.ts
@@ -1,3 +1,4 @@
+/* --------------- 프로필 --------------- */
 // GET /api/v1/users/me
 interface MyUserInfo {
   id: string;
@@ -37,7 +38,7 @@ interface UserProfileDetail {
   regions: Region[];
   positions: PositionAndLevel[];
   genres: Genre[];
-  recent_posts: Post[];
+  recent_posts: Pick<Post, "id" | "title" | "created_at">[];
   recent_comments: Comment[];
 }
 
@@ -45,12 +46,6 @@ export interface Comment {
   id: string;
   content: string;
   post: Pick<Post, "id" | "title">;
-  created_at: Date;
-}
-
-export interface Post {
-  id: string;
-  title: string;
   created_at: Date;
 }
 
@@ -76,5 +71,34 @@ interface Genre {
 
 interface ExperienceLevel {
   id: string;
+  name: string;
+}
+
+/* --------------- 구인/구직 --------------- */
+// GET /api/v1/recruiting-posts
+export interface PostList {
+  next_cursor: string;
+  posts: Post[];
+}
+
+export interface Post {
+  id: string;
+  title: string;
+  author: Author;
+  is_bookmarked: boolean;
+  post_type: PostType;
+  regions: PostType[];
+  created_at: Date;
+  views_count: number;
+  comments_count: number;
+  is_closed: boolean;
+}
+
+export interface Author {
+  user_id: string;
+  nickname: string;
+}
+
+export interface PostType {
   name: string;
 }
