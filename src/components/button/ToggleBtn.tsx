7c13d7a530ebb255c6af6011cbe6d0608711ad06
Jun <nbc7799@naver.com>
2025-08-07 16:26:45 +0900

    #65 feat/toggle 사이즈 조정

--- src/components/button/ToggleBtn.tsx
+++ src/components/button/ToggleBtn.tsx
@@ -18,15 +18,15 @@ export default function ToggleBtn({ onToggle, defaultOn = false, className }: To
 
   return (
     <button onClick={handleClick} className={clsx("cursor-pointer", className)} aria-pressed={isOn}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="24" viewBox="0 0 64 24">
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="24" viewBox="0 0 64 24">
         <rect
-          width="64"
+          width="54"
           height="24"
           rx="12"
           className={`transition-colors duration-300 ease-in-out ${isOn ? "fill-primary-thick" : "fill-neutral-400"}`}
         />
         <circle
-          cx={isOn ? "51" : "13"}
+          cx={isOn ? "41" : "13"}
           cy="12"
           r="10"
           className="transition-all duration-300 ease-in-out fill-white"
