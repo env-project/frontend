2969469d2b639226754b10582576e55c5801b98b
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-08 13:15:38 +0900

    #78 feat: 모달이 화면 중앙에 배치

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -61,7 +61,7 @@ function ModalContent({ children, className, ...props }: ModalContentProps) {
     <div
       className={cn(
         isOpen ? "visible" : "hidden",
-        "size-10 border-neutral-600 border-2",
+        "p-5 border-neutral-600 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
         className
       )}
       {...props}
