bb9c8534a46a80452d0edaa012adb3c8d8ec2774
Jun <nbc7799@naver.com>
2025-08-08 14:51:39 +0900

    #85 feature/common-toggle버튼

--- src/components/button/ToggleBtn.tsx
+++ src/components/button/ToggleBtn.tsx
@@ -1,23 +1,39 @@
 import { cn } from "@/libs/utils";
-import { useState } from "react";
+import { useState, type ComponentProps } from "react";
 
-interface ToggleBtnProps {
-  onToggle?: (isOn: boolean) => void;
+interface ToggleBtnProps extends ComponentProps<"button"> {
+  onToggleChange?: (isOn: boolean) => void;
   defaultOn?: boolean;
   className?: string;
 }
 
-export default function ToggleBtn({ onToggle, defaultOn = false, className }: ToggleBtnProps) {
+export default function ToggleBtn({
+  //토글상태 변경시 호출 함수
+  onToggleChange,
+  //컴포넌트 초기상태
+  defaultOn = false,
+  className,
+  ...props
+}: ToggleBtnProps) {
   const [isOn, setIsOn] = useState(defaultOn);
 
   const handleClick = () => {
-    const newState = !isOn;
-    setIsOn(newState);
-    onToggle?.(newState);
+    setIsOn((prev) => {
+      const newState = !prev;
+      //부모 컴포넌트로 상태 공유
+      onToggleChange?.(newState);
+      return newState;
+    });
   };
 
   return (
-    <button onClick={handleClick} className={cn("cursor-pointer", className)} aria-pressed={isOn}>
+    <button
+      onClick={handleClick}
+      className={cn("cursor-pointer", className)}
+      aria-pressed={isOn}
+      aria-label={isOn ? "활성화됨" : "비활성화됨"}
+      {...props}
+    >
       <svg xmlns="http://www.w3.org/2000/svg" width="64" height="24" viewBox="0 0 64 24">
         <rect
           width="64"
