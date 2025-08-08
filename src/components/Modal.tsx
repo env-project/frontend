1071d50e18b88aadccb3afb0d6b488a97a88c5f4
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-08 13:47:18 +0900

    #78 feat: 모달에 x버튼

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,6 +7,9 @@ import {
   type ReactElement,
 } from "react";
 import { cn } from "@/libs/utils";
+import Button from "./Button";
+import Text from "./text/Text";
+import H3 from "./text/H3";
 
 /* --------------------
    Context
@@ -54,17 +57,30 @@ interface ModalContentProps extends ComponentProps<"div"> {
 }
 
 function ModalContent({ children, className, ...props }: ModalContentProps) {
-  const { isOpen } = useModalContext();
+  const { isOpen, toggle } = useModalContext();
+
+  if (!isOpen) return null;
+
   return (
     <div
       className={cn(
         isOpen ? "visible" : "hidden",
-        "p-5 border-neutral-600 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+        "flex flex-col  min-w-64 border-primary-soft bg-bg-primary border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
         className
       )}
       {...props}
     >
-      {children}
+      <div className="w-full flex items-center justify-end">
+        <H3
+          className=" text-text-primary hover:cursor-pointer p-1"
+          onClick={() => {
+            toggle();
+          }}
+        >
+          X
+        </H3>
+      </div>
+      <div className="w-full p-2">{children}</div>
     </div>
   );
 }
