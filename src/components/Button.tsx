76cc16e48b36c22a2863a67b00d21b0ecd677261
Jun <nbc7799@naver.com>
2025-08-11 15:50:04 +0900

    #94 featrue/linkbutton 공용button에 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,9 @@
 import { cn } from "@/libs/utils";
-import type { ComponentProps, ReactNode } from "react";
+import type { ComponentProps, ElementType, ReactNode } from "react";
 
-interface ButtonProps extends ComponentProps<"button"> {
-  variant?: "default" | "outline";
+interface ButtonProps<T extends ElementType = "button"> {
+  as?: T;
+  variant?: "default" | "outline" | "link-primary" | "link-secondary";
   color?:
     | "primary"
     | "primary-thick"
@@ -11,8 +12,11 @@ interface ButtonProps extends ComponentProps<"button"> {
     | "secondary-thick"
     | "secondary-soft"
     | "error"
-    | "neutral";
+    | "neutral"
+    | "neutral-soft";
   children: ReactNode;
+  className?: string;
+  to?: string;
 }
 
 const getVariantClasses = (variant: ButtonProps["variant"], color: ButtonProps["color"]) => {
@@ -26,7 +30,20 @@ const getVariantClasses = (variant: ButtonProps["variant"], color: ButtonProps["
       "border-secondary-soft": color === "secondary-soft",
       "border-error": color === "error",
       "border-neutral-600": color === "neutral",
+      "border-neutral-300": color === "neutral-soft",
     });
+  }
+
+  if (variant === "link-primary") {
+    return cn(
+      "bg-primary-thick text-text-on-dark border-transparent hover:bg-white hover:border-1 hover:text-text-primary hover:border-primary-thick"
+    );
+  }
+
+  if (variant === "link-secondary") {
+    return cn(
+      "bg-white border border-transparent hover:text-text-primary hover:border-1 hover:border-primary-thick"
+    );
   }
 
   return cn({
@@ -38,26 +55,30 @@ const getVariantClasses = (variant: ButtonProps["variant"], color: ButtonProps["
     "bg-secondary-soft": color === "secondary-soft",
     "bg-error": color === "error",
     "bg-neutral-600": color === "neutral",
+    "bg-neutral-300": color === "neutral-soft",
   });
 };
 
-export default function Button({
+export default function Button<T extends ElementType = "button">({
+  as,
   variant = "default",
   color = "primary",
   className,
   children,
   ...rest
-}: ButtonProps) {
+}: ButtonProps<T> & Omit<ComponentProps<T>, keyof ButtonProps>) {
+  const Component = as || "button";
+
   return (
-    <button
+    <Component
       className={cn(
-        "px-5 py-2 rounded-xl transition-all active:scale-95 hover:cursor-pointer hover:scale-105 focus:outline-none",
+        "flex items-center justify-center px-6 py-2 rounded-md transition-all active:scale-95 hover:cursor-pointer hover:scale-105 focus:outline-none duration-300 ease-in-out",
         getVariantClasses(variant, color),
         className
       )}
       {...rest}
     >
       {children}
-    </button>
+    </Component>
   );
 }
