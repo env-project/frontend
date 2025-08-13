44e45d5fe678960aca79b93549c98f78d3816b1b
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-13 10:46:24 +0900

    #77 feat: 삭제 모달

--- src/components/CommentUI.tsx
+++ src/components/CommentUI.tsx
@@ -3,6 +3,9 @@ import defaultUserImage from "@/assets/images/user-default-image.png";
 import Text from "@/components/text/Text";
 import Badge from "@/components/Badge";
 import { BsArrowReturnRight } from "react-icons/bs";
+import { Modal, ModalClose, ModalContent, ModalTrigger } from "@/components/Modal";
+import Button from "@/components/Button";
+import H3 from "@/components/text/H3";
 interface CommentUIProps {
   comment: Comment;
   isChild?: boolean;
@@ -14,6 +17,7 @@ export default function CommentUI({ comment, isChild = false }: CommentUIProps) 
     is_owner: isOwner,
     content,
     children,
+    id,
   } = comment;
 
   return (
@@ -51,12 +55,7 @@ export default function CommentUI({ comment, isChild = false }: CommentUIProps) 
                   >
                     수정
                   </Text>
-                  <Text
-                    variant="label"
-                    className="text-text-primary cursor-pointer hover:underline"
-                  >
-                    삭제
-                  </Text>
+                  <CommentDeleteModal commentId={id} />
                 </>
               ) : null}
             </div>
@@ -67,8 +66,51 @@ export default function CommentUI({ comment, isChild = false }: CommentUIProps) 
         </div>
       </div>
       {children.map((child) => (
-        <CommentUI isChild={true} comment={child} />
+        <CommentUI isChild={true} comment={child} key={child.id} />
       ))}
     </>
+  );
+}
+
+interface CommentDeleteModalProps {
+  commentId: string;
+}
+
+function CommentDeleteModal({ commentId }: CommentDeleteModalProps) {
+  //TODO: 실제 api 연결
+  const handleDeleteClick = () => {
+    console.log(commentId);
+  };
+
+  return (
+    <Modal>
+      <ModalTrigger className="flex justify-center items-center">
+        <Text variant="label" className="text-text-primary cursor-pointer hover:underline">
+          삭제
+        </Text>
+      </ModalTrigger>
+      <ModalContent>
+        <div className="flex flex-col p-2 space-y-5">
+          <H3 className="text-text-primary">댓글을 삭제하시겠습니까?</H3>
+          <div className="flex w-full justify-end space-x-2">
+            <ModalClose>
+              <Button variant="outline" color="error">
+                <Text variant="subText" className="text-text-primary">
+                  취소
+                </Text>
+              </Button>
+            </ModalClose>
+
+            <ModalClose>
+              <Button variant="default" color="error" onClick={handleDeleteClick}>
+                <Text variant="subText" className="text-text-on-dark">
+                  삭제
+                </Text>
+              </Button>
+            </ModalClose>
+          </div>
+        </div>
+      </ModalContent>
+    </Modal>
   );
 }
