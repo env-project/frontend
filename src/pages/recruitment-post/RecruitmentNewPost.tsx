8fd37c6061637297056e8af87d632d647c86fb94
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-13 15:40:44 +0900

    #122 feat: 구인글 작성용 zod 스키마

--- src/pages/recruitment-post/RecruitmentNewPost.tsx
+++ src/pages/recruitment-post/RecruitmentNewPost.tsx
@@ -1,3 +1,30 @@
+import z from "zod";
+
+export const positionItemSchema = z.object({
+  position_id: z.string(),
+  experienced_level_id: z.string(),
+});
+
+const recruitmentPostSchema = z.object({
+  title: z.string().min(1).max(30),
+  content: z.string().min(1),
+
+  imageUrl: z.string().optional(),
+  bandName: z.string().optional(),
+  bandComposition: z.string().optional(),
+  activityTime: z.string().optional(),
+  applicationMethod: z.string().optional(),
+  practiceFrequencyTime: z.string().optional(),
+  otherConditions: z.string().optional(),
+
+  orientationId: z.string().optional(),
+  recruitmentTypeId: z.string().optional(),
+
+  regionIds: z.array(z.string()).optional(),
+  genreIds: z.array(z.string()).optional(),
+  positions: z.array(positionItemSchema).optional(),
+});
+
 export default function RecruitmentNewPost() {
   return <div>RecruitmentNewPost</div>;
 }
