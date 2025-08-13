8eb2bd66a8bab01254c2ec9341d972a293bf4511
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-13 17:05:27 +0900

    #122 feat: 단순 문자열 input react-hook-form 연결

--- src/pages/recruitment-post/RecruitmentNewPost.tsx
+++ src/pages/recruitment-post/RecruitmentNewPost.tsx
@@ -2,6 +2,8 @@ import z from "zod";
 import Input from "@/components/input/AuthInput";
 import type { ReactNode } from "react";
 import ImageInput from "@/components/input/ImageInput";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const positionItemSchema = z.object({
   position_id: z.string(),
@@ -9,7 +11,7 @@ const positionItemSchema = z.object({
 });
 
 const recruitmentPostSchema = z.object({
-  title: z.string().min(1).max(30),
+  title: z.string().min(1, "1글자 이상").max(30, "30이하"),
   content: z.string().min(1),
 
   imageUrl: z.string().optional(),
@@ -29,59 +31,73 @@ const recruitmentPostSchema = z.object({
   positions: z.array(positionItemSchema).optional(),
 });
 
+type TRecruitmentPostSchema = z.infer<typeof recruitmentPostSchema>;
+
 export default function RecruitmentNewPost() {
+  const { register } = useForm<TRecruitmentPostSchema>({
+    resolver: zodResolver(recruitmentPostSchema),
+  });
+
   return (
     <div>
       RecruitmentNewPost
       <form className="flex flex-col items-center jsutify-start space-y-1 w-full p-2 max-w-lg">
         <InputWithLabelContainer>
           <label htmlFor="title">제목*</label>
-          <Input id="title" className="w-full" />
+          <Input id="title" className="w-full" {...register("title")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="band-name">밴드 이름</label>
-          <Input id="band-name" className="w-full" />
+          <Input id="band-name" className="w-full" {...register("bandName")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="band-composition">밴드 구성</label>
-          <Input id="band-composition" className="w-full" />
+          <Input id="band-composition" className="w-full" {...register("bandComposition")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="activity-time">주 활동 시간</label>
-          <Input id="activity-time" className="w-full" />
+          <Input id="activity-time" className="w-full" {...register("activityTime")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="practice-frequency-time">활동 간격</label>
-          <Input id="practice-frequency-time" className="w-full" />
+          <Input
+            id="practice-frequency-time"
+            className="w-full"
+            {...register("practiceFrequencyTime")}
+          />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="contact-info">연락 방법</label>
-          <Input id="contact-info" className="w-full" />
+          <Input id="contact-info" className="w-full" {...register("contactInfo")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="application-method">지원 방법</label>
-          <Input id="application-method" className="w-full" />
+          <Input id="application-method" className="w-full" {...register("applicationMethod")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
           <label htmlFor="other-conditions">기타 조건</label>
-          <Input id="other-conditions" className="w-full" />
+          <Input id="other-conditions" className="w-full" {...register("otherConditions")} />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
-          <label htmlFor="title">대표 이미지</label>
-          <ImageInput />
+          <label htmlFor="image">대표 이미지</label>
+          <ImageInput id="image" />
         </InputWithLabelContainer>
 
         <InputWithLabelContainer>
-          <label htmlFor="title">자세한 설명*</label>
-          <textarea id="title" className="w-full h-32 p-2 rounded-xl border" />
+          <label htmlFor="content">자세한 설명*</label>
+          <textarea
+            id="content"
+            className="w-full h-32 p-2 rounded-xl border"
+            {...register("content")}
+          />
         </InputWithLabelContainer>
       </form>
     </div>
