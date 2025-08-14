ac5f3f4e2de1fad451d5b1fdfc1d823b780224ac
Jun <nbc7799@naver.com>
2025-08-14 14:16:59 +0900

    #130 feat/home페이지

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
+import HeroBanner from "@/components/HeroBanner";
+import Text from "@/components/text/Text";
+
+import ProfileList from "./profile/ProfileList";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <HeroBanner />
+      <section>
+        <Text>인기 구인글</Text>
+        <ProfileList />
+      </section>
+    </>
+  );
 }
