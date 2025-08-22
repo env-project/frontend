d857bd0f86a4812b746618e5d23562b382bde885
Jun <nbc7799@naver.com>
2025-08-22 11:51:37 +0900

    #130 feature: rebase커밋

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import HeroBanner from "@/components/HeroBanner";
-import ProfileList from "./profile/ProfileList";
+import RecruitmentList from "./recruitment-post/RecruitmentList";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
-      <ProfileList />
+      <RecruitmentList />
     </>
   );
 }
