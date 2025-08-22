6b056c8a3662e5d8e85deb6ca17ac637030d9a69
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-22 14:43:46 +0900

    #189 feat: 구인글 상세 api 연결

--- src/pages/recruitment-post/RecruitmentDetail.tsx
+++ src/pages/recruitment-post/RecruitmentDetail.tsx
@@ -7,89 +7,15 @@ import Text from "@/components/text/Text";
 import { getTimeDiff } from "@/libs/utils";
 import type { CommentList } from "@/types/api-res-comment";
 import type { PostDetail } from "@/types/api-res-recruitment";
-import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
 import EyeIcon from "@/components/icons/EyeIcon";
 import CommentIcon from "@/components/icons/CommentIcon";
 import BookmarkIcon from "@/components/icons/BookmarkIcon";
 import Button from "@/components/Button";
 import TogglePostStatusModal from "@/components/TogglePostStatusModal";
-
-//더미 포스트 데이터 실제론 api 준비
-const dummyPostData: PostDetail = {
-  id: "1",
-  title: "유지민 밴드에서 키보드를 찾습니다!",
-  author: {
-    id: "1111",
-    nickname: "이재현",
-  },
-  is_closed: false,
-  is_owner: true,
-  is_bookmarked: true,
-
-  created_at: "2025-08-12T11:00:00Z",
-
-  views_count: 100,
-  comments_count: 10,
-  bookmarks_count: 5,
-
-  content:
-    "서울 홍대에서 활동 하고 있는 유지빈 밴드입니다. 취미 지향 직장인 밴드입니다. 합주는 주로 주말에 격주로 진행합니다. 멤버는 현재 남보컬 겸 세컨 기타, 리드 기타, 드럼, 베이스로 이루어져 있습니다. 관심 있으시면 댓글이나 아래 이메일로 편하게 연락주세요! example@example.com",
-
-  band_name: "유지민 밴드",
-  band_composition: "남보컬 겸 세컨 기타, 리드 기타, 드럼, 베이스",
-  activity_time: "월요일 오후 8시, 주말",
-  practice_frequency_time: "격주로 주 1회",
-  contact_info: "examle@example.com",
-  application_method: "대면 오디션",
-  other_conditions: "공연 경험 있으면 우대",
-
-  recruitment_type: { id: "asdfadsfa", name: "고정 밴드" },
-
-  genres: [
-    {
-      id: "143125235",
-      name: "인디락",
-    },
-    {
-      id: "14313asdfa25235",
-      name: "하드락",
-    },
-  ],
-
-  orientation: {
-    id: "asdfagsdfjoppjo",
-    name: "취미",
-  },
-
-  positions: [
-    {
-      position_id: "1222",
-      position_name: "키보드",
-      experience_level_id: "312345",
-      experience_level_name: "취미 1년 이상 3년 이하",
-    },
-    {
-      position_id: "12234t622",
-      position_name: "여보컬",
-      experience_level_id: "312315545",
-      experience_level_name: "취미 1년 이상 3년 이하",
-    },
-  ],
-
-  regions: [
-    {
-      id: "12415r123t5234",
-      name: "서울 서부",
-    },
-    {
-      id: "12r123t5234",
-      name: "서울 남부",
-    },
-  ],
-
-  image_url: "https://dummyimage.com/600x400/000/fff​",
-};
+import { useQuery } from "@tanstack/react-query";
+import api from "@/libs/axios";
+import LoadingOverlay from "@/components/loading/LoadingOverlay";
 
 //실제론 api로 호출
 const dummyCommenList: CommentList = {
@@ -148,6 +74,35 @@ export default function RecruitmentDetail() {
   const { postId } = useParams();
 
   const {
+    data: postData,
+    isPending,
+    isError,
+  } = useQuery<PostDetail>({
+    queryKey: ["recruitment", "detail", postId],
+    queryFn: async () => {
+      const res = await api.get(`/recruiting/${postId}`);
+
+      return res.data;
+    },
+  });
+
+  if (!postId) {
+    return <div>해당 게시물은 삭제되었습니다.</div>;
+  }
+
+  if (isPending) {
+    return <LoadingOverlay />;
+  }
+
+  if (isError) {
+    return (
+      <H1 className="text-text-primary bg-bg-primary w-full min-h-full">
+        데이터를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.
+      </H1>
+    );
+  }
+
+  const {
     title,
     is_bookmarked: isBookmarked,
     author: { id: userId, nickname },
@@ -171,20 +126,10 @@ export default function RecruitmentDetail() {
     comments_count: commentsCount,
     bookmarks_count: bookmarksCount,
     is_owner: isOwner,
-  } = dummyPostData;
-
-  const [timeDiff, setTimeDiff] = useState("");
-
-  useEffect(() => {
-    setTimeDiff(getTimeDiff(new Date(createdAt)));
-  }, [createdAt, setTimeDiff]);
-
-  if (!postId) {
-    return <div>해당 게시물은 삭제되었습니다.</div>;
-  }
+  } = postData;
 
   return (
-    <div className="bg-bg-primary text-text-primary p-2 flex justify-center ">
+    <div className="bg-bg-primary text-text-primary p-2 flex justify-center">
       <div className="flex flex-col gap-3 items-center max-w-full min-w-0 ">
         <div className="flex flex-col max-w-2xl min-w-0">
           <div className="flex max-w-full min-w-0 justify-between items-center">
@@ -212,7 +157,7 @@ export default function RecruitmentDetail() {
             <div>
               <Text variant="subText">{nickname}</Text>
               <Text variant="subText">·</Text>
-              <Text variant="subText">{`${timeDiff}`}</Text>
+              <Text variant="subText">{`${getTimeDiff(new Date(createdAt))}`}</Text>
             </div>
           </div>
 
@@ -230,7 +175,7 @@ export default function RecruitmentDetail() {
                 {positions.map((position, index) => {
                   const {
                     position_name: positionName,
-                    experience_level_name: experienceLevelName,
+                    experienced_level_name: experienceLevelName,
                   } = position;
                   return (
                     <div
