da5a1d7caf534071e7c3982e8550c225135423a9
Jun <nbc7799@naver.com>
2025-08-22 16:19:44 +0900

    130 feature:인기프로필목록 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroBanner from "@/components/HeroBanner";
-import PopularRecruitmentList from "./recruitment-post/PopularRecruitmentList";
+import PopularRecruitmentList from "@/pages/recruitment-post/PopularRecruitmentList";
+import PopularProfileList from "@/pages/profile/PopularProfileList";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col gap-10">
       <HeroBanner />
       <PopularRecruitmentList />
-    </>
+      <PopularProfileList />
+    </div>
   );
 }
--- src/pages/profile/PopularProfileList.tsx
+++ src/pages/profile/PopularProfileList.tsx
@@ -1,30 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
-import type { PostList } from "@/types/api-res-recruitment";
 import { useEffect } from "react";
 import api from "@/libs/axios";
 import { useSearchParams } from "react-router";
 import { useDebounce } from "@/hooks/useDebounce";
-import RecruitmentCard from "@/components/RecruitmentCard";
 import LoadingOverlay from "@/components/loading/LoadingOverlay";
+import ProfileCard from "@/components/ProfileCard";
+import type { UserList } from "@/types/api-res-profile";
 
-export default function PopularRecruitmentList() {
+export default function PopularProfileList() {
   const [searchParams] = useSearchParams();
   const debouncedSearchParams = useDebounce(searchParams);
 
-  const { data, isPending, refetch } = useQuery<PostList>({
-    queryKey: ["popular-recruitment-list", { sort_by: "views", limit: 20 }],
+  const { data, isPending, refetch } = useQuery<UserList>({
+    queryKey: ["popular-Profile-list", { sort_by: "views", limit: 20 }],
     queryFn: async () => {
-      const res = await api.get("/recruiting", {
+      const res = await api.get("/profiles", {
         params: {
           sort_by: "views",
           limit: 20,
         },
       });
+      console.log(res.data);
       return res.data;
     },
   });
 
   useEffect(() => {
+    console.log("apiProfile", data);
     console.log(debouncedSearchParams.toString());
     refetch();
   }, [debouncedSearchParams]);
@@ -33,19 +35,19 @@ export default function PopularRecruitmentList() {
     return <LoadingOverlay />;
   }
 
-  if (!data || !data.posts || data.posts.length === 0) {
+  if (!data || !data.profiles || data.profiles.length === 0) {
     return (
       <div>
-        <h1>인기 구인글이 없습니다.</h1>
+        <h1>인기 프로필이 없습니다.</h1>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen gap-2 p-4 text-text-primary">
+    <div className="flex flex-col items-center justify-start min-h-screen gap-2 p-4 bg-bg-primary text-text-primary">
       <div className="flex flex-wrap items-center justify-center w-full gap-2">
-        {data.posts.map((post) => (
-          <RecruitmentCard postData={post} key={post.id} className="h-64" />
+        {data.profiles.map((post) => (
+          <ProfileCard profile={post} key={post.user_id} />
         ))}
       </div>
     </div>
