02c5d3fd1a69b61326a5e25a0ee61938b86036e5
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-22 13:46:16 +0900

    #182 feat: search params 적용

--- src/pages/recruitment-post/RecruitmentList.tsx
+++ src/pages/recruitment-post/RecruitmentList.tsx
@@ -3,18 +3,29 @@ import LoadingOverlay from "@/components/loading/LoadingOverlay";
 import RecruitmentCard from "@/components/RecruitmentCard";
 import H1 from "@/components/text/H1";
 import { useUserInfo } from "@/hooks/api/useUserInfo";
+import { useDebounce } from "@/hooks/useDebounce";
 import api from "@/libs/axios";
 import type { PostList } from "@/types/api-res-recruitment";
 import { useQuery } from "@tanstack/react-query";
-import type { AxiosResponse } from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router";
 
 export default function RecruitmentList() {
-  const { data, isPending } = useQuery<AxiosResponse<PostList>>({
-    queryKey: ["recruiment-list"],
-    queryFn: () => {
-      return api.get("/recruiting");
+  const [searchParams] = useSearchParams();
+  const debouncedSearchParams = useDebounce(searchParams);
+
+  const { data, isPending, refetch } = useQuery<PostList>({
+    queryKey: ["recruiment-list", debouncedSearchParams.toString()],
+    queryFn: async () => {
+      const res = await api.get("/recruiting", { params: debouncedSearchParams });
+      return res.data;
     },
   });
+
+  useEffect(() => {
+    console.log(debouncedSearchParams.toString());
+    refetch();
+  }, [debouncedSearchParams, refetch]);
 
   const user = useUserInfo();
 
@@ -25,7 +36,7 @@ export default function RecruitmentList() {
         <LoadingOverlay />
       ) : data ? (
         <div className="flex flex-wrap gap-2 w-full justify-start items-center">
-          {data.data.posts.map((post) => (
+          {data.posts.map((post) => (
             <RecruitmentCard postData={post} key={post.id} className="h-64" />
           ))}
         </div>
