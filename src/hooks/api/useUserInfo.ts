6fd612dc1098ce869bf8c1cc7bb03dedc737259b
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-22 15:26:44 +0900

    #195 fix: useUserInfo 훅 수정

--- src/hooks/api/useUserInfo.ts
+++ src/hooks/api/useUserInfo.ts
@@ -1,23 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "@/libs/axios";
 import type { MyUserInfo } from "@/types/api-res-profile";
-import type { AxiosResponse } from "axios";
 
-async function fetchUserInfo(): Promise<AxiosResponse<MyUserInfo>> {
+async function fetchUserInfo(): Promise<MyUserInfo> {
   const res = await api.get<MyUserInfo>("/users/me");
-  return res;
+  return res.data;
 }
 
 export function useUserInfo() {
-  const { data: res, status } = useQuery({
+  return useQuery<MyUserInfo>({
     queryKey: ["user"],
     queryFn: fetchUserInfo,
     staleTime: 1000 * 60 * 5,
   });
-
-  if (status === "success") {
-    return res.data;
-  } else {
-    return false;
-  }
 }
