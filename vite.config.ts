817d33f0a87ab35aa932683af0575735f0c15251
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-24 18:33:45 +0900

    #205 feat: https 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: isDev
+      ? {
+          https: {
+            key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+            cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+          },
+          host: "localhost",
+          port: 5173,
+        }
+      : undefined,
+  };
 });
