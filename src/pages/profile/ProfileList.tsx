09fb8b1ed86f3be201552b883af5d94fb45525e7
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-26 00:17:12 +0900

    #224 feat: profile list api 연결

--- src/pages/profile/ProfileList.tsx
+++ src/pages/profile/ProfileList.tsx
@@ -1,16 +1,14 @@
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 import Filter from "@/components/Filter";
 import ProfileCard from "@/components/ProfileCard";
 import { ProfileListLoading } from "@/components/skeletons/Loaders";
-import InlineDots from "@/components/loading/InlineDots";
-import { useInfinite } from "@/hooks/useInfinite";
+
 import { useSearchParams } from "react-router";
-import { buildProfilesQueryFromSearchParams } from "@/libs/buildProfilesquery";
-import { type UserList, type UserProfile } from "@/types/api-res-profile";
-import { fetchProfilesInfinitePage } from "@/api/fetchProfilesInfinite";
+import type { UserList } from "@/types/api-res-profile";
 import { useQuery } from "@tanstack/react-query";
 import { useDebounce } from "@/hooks/useDebounce";
 import api from "@/libs/axios";
+import H1 from "@/components/text/H1";
 
 const GRID =
   "grid gap-4 sm:gap-6 lg:gap-8 justify-items-center [grid-template-columns:repeat(auto-fit,minmax(280px,1fr))]";
@@ -31,37 +29,37 @@ function TwoColLayout({ children }: { children: React.ReactNode }) {
 export default function ProfileList() {
   const [searchParams] = useSearchParams();
   const debouncedSearchParams = useDebounce(searchParams);
-  const query = buildProfilesQueryFromSearchParams(searchParams);
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
-    useInfinite<UserProfile>({
-      queryKey: ["profiles", query],
-      fetchPage: ({ pageParam }) => fetchProfilesInfinitePage({ pageParam, query }),
-    });
+  // const query = buildProfilesQueryFromSearchParams(searchParams);
+  // const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+  //   useInfinite<UserProfile>({
+  //     queryKey: ["profiles", query],
+  //     fetchPage: ({ pageParam }) => fetchProfilesInfinitePage({ pageParam, query }),
+  //   });
 
-  const loaderRef = useRef<HTMLDivElement | null>(null);
+  // const loaderRef = useRef<HTMLDivElement | null>(null);
 
   // 마지막 센티널에 닿으면 다음 페이지 로드
-  useEffect(() => {
-    if (!loaderRef.current) return;
-    const el = loaderRef.current;
+  // useEffect(() => {
+  //   if (!loaderRef.current) return;
+  //   const el = loaderRef.current;
 
-    const io = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
-          fetchNextPage();
-        }
-      },
-      { rootMargin: "120px" }
-    );
+  //   const io = new IntersectionObserver(
+  //     (entries) => {
+  //       if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+  //         fetchNextPage();
+  //       }
+  //     },
+  //     { rootMargin: "120px" }
+  //   );
 
-    io.observe(el);
-    return () => io.unobserve(el);
-  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
+  //   io.observe(el);
+  //   return () => io.unobserve(el);
+  // }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
-  const pages = data?.pages ?? [];
-  const profiles = pages.flatMap((p) => p.items);
+  // const pages = data?.pages ?? [];
+  // const profiles = pages.flatMap((p) => p.items);
 
-  const {} = useQuery<UserList>({
+  const { data, isPending, refetch } = useQuery<UserList>({
     queryKey: ["profile", debouncedSearchParams],
     queryFn: async () => {
       const res = await api.get("/profiles", { params: debouncedSearchParams });
@@ -69,24 +67,36 @@ export default function ProfileList() {
     },
   });
 
+  useEffect(() => {
+    refetch();
+  }, [debouncedSearchParams, refetch]);
+
   return (
     <TwoColLayout>
       <div className={GRID}>
         {/* 1) 첫 페이지 로딩 때만 스켈레톤 전체 렌더 */}
-        {isLoading ? (
+        {isPending ? (
           <ProfileListLoading count={9} />
         ) : (
           <>
             {/* 2) 누적 카드 렌더 */}
-            {profiles.map((profile) => (
-              <ProfileCard key={profile.user_id} profile={profile} />
-            ))}
+            {data ? (
+              data.profiles.length > 0 ? (
+                data.profiles.map((profile) => (
+                  <ProfileCard key={profile.user_id} profile={profile} />
+                ))
+              ) : (
+                <H1>조건에 맞는 데이터가 없습니다.</H1>
+              )
+            ) : (
+              <H1>데이터를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.</H1>
+            )}
+            {/* 
+           3) 하단 인디케이터(다음 페이지 로딩 중에만 표시)
+            isFetchingNextPage && <InlineDots />
 
-            {/* 3) 하단 인디케이터(다음 페이지 로딩 중에만 표시) */}
-            {isFetchingNextPage && <InlineDots />}
-
-            {/* 4) 센티널: 보이기만 하면 다음 페이지 로드 트리거 */}
-            {hasNextPage && <div ref={loaderRef} className="col-span-full h-4" />}
+             4) 센티널: 보이기만 하면 다음 페이지 로드 트리거 
+            hasNextPage && <div ref={loaderRef} className="col-span-full h-4" /> */}
           </>
         )}
       </div>
