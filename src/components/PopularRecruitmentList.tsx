6f4a9049cd3ea636150b0d15833903b34a6cd97d
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-25 13:52:00 +0900

    #207 fix:인기 구인글 리스트 레이아웃 수정

--- src/components/PopularRecruitmentList.tsx
+++ src/components/PopularRecruitmentList.tsx
@@ -3,6 +3,8 @@ import { usePopularRecruitment } from "@/hooks/queries/usePopularRecruitment";
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import InlineSpinner from "./loading/InlineSpinner";
+import useWindowWidth from "@/hooks/useWindowWidth";
+import { cn } from "@/libs/utils";
 
 export default function PopularRecruitmentList() {
   const { data, isPending } = usePopularRecruitment({ sortBy: "views", limit: 20 });
@@ -11,12 +13,14 @@ export default function PopularRecruitmentList() {
     loop: true,
     slides: { perView: 3, spacing: 15 },
     breakpoints: {
-      "(max-width: 640px)": { slides: { perView: 1, spacing: 10 } },
-      "(min-width: 641px) and (max-width: 1024px)": { slides: { perView: 3, spacing: 15 } },
-      "(min-width: 1025px)": { slides: { perView: 4, spacing: 20 } },
-      "(min-width: 1545px)": { slides: { perView: 5, spacing: 20 } },
+      "(max-width: 700px)": { slides: { perView: 1, spacing: 10 } },
+      "(min-width: 701px) and (max-width: 1024px)": { slides: { perView: 2, spacing: 10 } },
+      "(min-width: 1025px)": { slides: { perView: 3, spacing: 10 } },
+      "(min-width: 1545px)": { slides: { perView: 4, spacing: 10 } },
     },
   });
+
+  const width = useWindowWidth();
 
   if (isPending) {
     return <InlineSpinner />;
@@ -32,18 +36,21 @@ export default function PopularRecruitmentList() {
 
   return (
     <div className="flex items-center justify-start text-text-primary">
-      <button onClick={() => slider.current?.prev()} className="px-2 ">
-        ◀
+      <button onClick={() => slider.current?.prev()} className={cn(width > 420 ? "px-2" : "")}>
+        {width > 420 ? "◀" : ""}
       </button>
       <div ref={sliderRef} className="w-full px-4 keen-slider">
         {data.posts.map((post) => (
-          <div key={post.id} className="keen-slider__slide">
-            <RecruitmentCard postData={post} className="h-68 w-full" />
+          <div key={post.id} className="keen-slider__slide max-w-80">
+            <RecruitmentCard
+              postData={post}
+              className={cn("h-76 hover:shadow-none", width > 420 ? "" : "w-60")}
+            />
           </div>
         ))}
       </div>
-      <button onClick={() => slider.current?.next()} className="px-2 ">
-        ▶
+      <button onClick={() => slider.current?.next()} className={cn(width > 420 ? "px-2" : "")}>
+        {width > 420 ? "▶" : ""}
       </button>
     </div>
   );
