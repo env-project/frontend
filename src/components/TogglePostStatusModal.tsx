557fd21d7a213c3ef32af598197be2a74239fbea
JaeHyunLee123 <jhyon123@gmail.com>
2025-08-25 15:43:00 +0900

    #191 feat: 마감하기 api 연결

--- src/components/TogglePostStatusModal.tsx
+++ src/components/TogglePostStatusModal.tsx
@@ -2,6 +2,9 @@ import Button from "@/components/Button";
 import { Modal, ModalClose, ModalContent, ModalTrigger } from "@/components/Modal";
 import H3 from "@/components/text/H3";
 import Text from "@/components/text/Text";
+import api from "@/libs/axios";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface TogglePostStatusModalProps {
   isClosed: boolean;
@@ -9,9 +12,22 @@ interface TogglePostStatusModalProps {
 }
 
 export default function TogglePostStatusModal({ isClosed, postId }: TogglePostStatusModalProps) {
+  const [apiError, setApiError] = useState("");
+
+  const { mutate } = useMutation({
+    mutationFn: ({ isClosed }: { isClosed: boolean }) => {
+      return api.patch(`/recruiting/${postId}/status?is_closed=${isClosed}`);
+    },
+    onSuccess: () => {
+      window.location.reload();
+    },
+    onError: () => {
+      setApiError("전송 중 문제가 발생했습니다. 잠시후 다시 시도해주세요.");
+    },
+  });
+
   const handleClick = () => {
-    //실제 api 연결
-    console.log(`${postId} 마감 상태 변경`);
+    mutate({ isClosed: !isClosed });
   };
 
   return (
@@ -25,6 +41,9 @@ export default function TogglePostStatusModal({ isClosed, postId }: TogglePostSt
         <H3 className="text-text-primary">
           {isClosed ? "다시 열시겠습니까?" : "마감하시겠습니까?"}
         </H3>
+        <Text variant="label" className="text-error">
+          {apiError}
+        </Text>
         <div className="flex justify-end items-center w-full space-x-2">
           <ModalClose>
             <Button variant="outline" color="error" type="button">
